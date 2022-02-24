8760bb7538645adcefe864ef2ac8d42f18e472de
Captain-G <poppinpites7@gmail.com>
2022-02-24 21:37:35 +0300

    Added octal functionality

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-print("This program converts decimal numbers to their binary equivalent")
-number = int(input("Enter number here : "))
 
+print("This program converts decimal numbers to their binary or octal equivalents")
+number = int(input("Enter number here : "))
+choice = input("Binary(B) or Octal(O)? : ").upper()
+
+oct = []
 bin = []
-for i in range(0, 100):
-    rem = number % 2
-    number = number // 2
-    bin.append(rem)
-    if number <= 0:
-        break
-bin.reverse()
-print(bin)
+if choice == "B":
+    for i in range(0, 100):
+        rem = number % 2
+        number = number // 2
+        bin.append(rem)
+        if number <= 0:
+            break
+    bin.reverse()
+    print(bin)
+elif choice == "O":
+    for i in range(0, 100):
+        rem = number % 8
+        number = number // 8
+        oct.append(rem)
+        if number <= 0:
+            break
+    oct.reverse()
+    print(oct)
+else:
+    print("Enter valid choice!")
+
 
